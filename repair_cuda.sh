8706f57263e255b24402de0bd289ae7d67fabde1
catid <mrcatid@gmail.com>
2023-05-28 19:43:20 +0000

    fix repair script

--- repair_cuda.sh
+++ repair_cuda.sh
@@ -3,6 +3,10 @@
 # Enable the 'errexit' option to make the script exit if a command fails
 set -e
 
+echo "Selecting auto version of GCC, since CUDA drivers do not work with GCC 11."
+
+ansible-playbook playbooks/select_gcc_auto.yml
+
 echo "Removing CUDA..."
 
 ansible-playbook playbooks/remove_cuda.yml
